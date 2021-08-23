f809bb5f40bd701c5347486e13f84c7b3a8686e7
bjosttveit <bjornost@stud.ntnu.no>
2021-08-23 20:24:58 +0200

    set up react project

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Fondet</h1>
+    </div>
+  );
+};
+
+export default App;
