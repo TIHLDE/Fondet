eef9010f89be52c94c31f97e7de125621a6b6849
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-12-15 12:35:12 +0100

    responsive af

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,10 +2,28 @@ import React, { useState } from 'react';
 import { AppBar, Box, Button, IconButton, Link, Toolbar, Typography, useScrollTrigger } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
+// Utils
+import useWindowSize from 'utils/useWindowSize';
+
 // Assets
 import logo from 'assets/logo.png';
 import { Link as RouterLink } from 'react-router-dom';
 import routes from 'constants/routes';
+
+const menu = [
+  {
+    link: routes.APPLY,
+    label: 'Søk om støtte',
+  },
+  {
+    link: routes.ABOUT,
+    label: 'Om fondet',
+  },
+  {
+    link: routes.GROUP,
+    label: 'Forvaltningsgruppen',
+  },
+];
 
 function ElevationScroll({ children, override }: { children: React.ReactElement; override?: boolean }) {
   const trigger = useScrollTrigger({
@@ -20,29 +38,37 @@ function ElevationScroll({ children, override }: { children: React.ReactElement;
 }
 
 const Header: React.FunctionComponent = () => {
-  // TODO: close drawer when resize window up
   const [drawerOpen, setDrawerOpen] = useState(false);
+
+  // close drawer if width exeeds breakpoint
+  const windowSize = useWindowSize();
+  if ((windowSize.width ?? 0) >= 900 && drawerOpen) {
+    setDrawerOpen(false);
+  }
 
   return (
     <ElevationScroll override={drawerOpen}>
       <AppBar>
         <Toolbar disableGutters style={{ justifyContent: 'center' }}>
-          <Link component={RouterLink} to={routes.HOME} underline='none' color='white' display='inherit' style={{ position: 'absolute', left: 0 }}>
+          <Link
+            component={RouterLink}
+            to={routes.HOME}
+            onClick={() => setDrawerOpen(false)}
+            underline='none'
+            color='white'
+            display='inherit'
+            style={{ position: 'absolute', left: 0 }}>
             <img height={40} style={{ marginLeft: 12 }} src={logo} />
             <Typography variant='h4' ml={2} fontWeight='bold'>
               Fondet
             </Typography>
           </Link>
           <Box ml={10} sx={{ display: { xs: 'none', md: 'inherit' } }}>
-            <Button component={RouterLink} to={routes.APPLY}>
-              Søk om støtte
-            </Button>
-            <Button component={RouterLink} to={routes.ABOUT}>
-              Om fondet
-            </Button>
-            <Button component={RouterLink} to={routes.GROUP}>
-              Forvaltningsgruppen
-            </Button>
+            {menu.map((item, i) => (
+              <Button key={i} component={RouterLink} to={item.link}>
+                {item.label}
+              </Button>
+            ))}
           </Box>
           <Box pr={1} sx={{ display: { xs: 'flex', md: 'none' } }} style={{ position: 'absolute', right: 0 }}>
             <IconButton size='large' onClick={() => setDrawerOpen(!drawerOpen)} color='inherit'>
@@ -55,15 +81,11 @@ const Header: React.FunctionComponent = () => {
           sx={{ display: { xs: 'flex', md: 'none' } }}
           alignItems='start'
           style={{ transition: 'height 0.25s', overflow: 'hidden', ...(drawerOpen ? { height: '100vh' } : { height: 0 }) }}>
-          <Button component={RouterLink} to={routes.APPLY} fullWidth>
-            Søk om støtte
-          </Button>
-          <Button component={RouterLink} to={routes.ABOUT} fullWidth>
-            Om fondet
-          </Button>
-          <Button component={RouterLink} to={routes.GROUP} fullWidth>
-            Forvaltningsgruppen
-          </Button>
+          {menu.map((item, i) => (
+            <Button key={i} component={RouterLink} to={item.link} onClick={() => setDrawerOpen(false)} fullWidth>
+              {item.label}
+            </Button>
+          ))}
         </Box>
       </AppBar>
     </ElevationScroll>
