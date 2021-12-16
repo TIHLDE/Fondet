3c4f27684fcb82aca4273d958eb5237360de5f4c
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-12-16 14:24:03 +0100

    pls

--- web/src/views/Apply/Apply.tsx
+++ web/src/views/Apply/Apply.tsx
@@ -4,7 +4,7 @@ const Apply: React.FunctionComponent = () => (
   <div>
     <p>Her kommer det informasjon om søknadsprosessen:</p>
     <ul>
-      <li>hvilke andre støtteordninger som finnes</li>
+      <li>Hvilke andre støtteordninger som finnes</li>
       <li>Lenke til søknadsmal</li>
       <li>Lenke til e-post addresse til fondet</li>
       <li>Oversikt over tidligere søknader</li>
