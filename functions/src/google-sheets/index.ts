e9eeb1585ee39c0ee10a3d378e547ded91adcc66
Bjørnar Østtveit <bjosttveit@gmail.com>
2022-01-16 23:18:23 +0100

    added file upload apps script and function

--- functions/src/google-sheets/index.ts
+++ functions/src/google-sheets/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { v4 as uuidv4 } from 'uuid';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import groupBy from '../util/groupBy';
 import { Application, Member, SheetsData } from './interfaces';
@@ -7,8 +8,43 @@ import { Application, Member, SheetsData } from './interfaces';
 const sheet_id = functions.config().google_sheets.sheet_id;
 const sa_client_email = functions.config().google_sheets.sa_client_email;
 const sa_private_key = functions.config().google_sheets.sa_private_key;
+const apps_script_key = functions.config().google_sheets.apps_script_key;
 
-export const updateSheetsData = functions.https.onRequest(async (_, res) => {
+function authenticatedRequest(handler: (req: functions.https.Request, resp: functions.Response) => void): functions.HttpsFunction {
+  return functions.https.onRequest((req, res) => {
+    if (req.headers.authorization !== `Bearer ${apps_script_key}`) {
+      functions.logger.error('UNAUTHORIZED');
+      res.sendStatus(401);
+    } else {
+      handler(req, res);
+    }
+  });
+}
+
+export const uploadFile = authenticatedRequest((req, res) => {
+  const contentType = req.headers['content-type'];
+  const data = req.rawBody;
+  const filename = `files/${uuidv4()}`;
+  const token = uuidv4();
+
+  const bucket = admin.storage().bucket();
+  const file = bucket.file(filename);
+  file
+    .save(data, {
+      contentType,
+      gzip: true,
+      metadata: {
+        firebaseStorageDownloadTokens: token,
+      },
+    })
+    .then(() => res.status(200).send(`https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(filename)}?alt=media&token=${token}`))
+    .catch((error) => {
+      functions.logger.error(error);
+      res.sendStatus(500);
+    });
+});
+
+export const updateSheetsData = authenticatedRequest(async (_, res) => {
   const doc = new GoogleSpreadsheet(sheet_id);
   doc.useServiceAccountAuth({
     client_email: sa_client_email,
