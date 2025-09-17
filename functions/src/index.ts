812727951a0edc5050be80e090e1b49d95016111
Anton Tveito <antontveitoo@gmail.com>
2025-09-17 20:58:13 +0800

    Clear repo

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase-admin';
-
-initializeApp();
-
-export * from './nordnet';
-export * from './google-sheets';
-export * from './fantasy-fond';
--- web/src/utils/ios.ts
+++
@@ -1,4 +0,0 @@
-export function isIOS(): boolean {
-  const userAgent = window.navigator.userAgent;
-  return Boolean(userAgent.match(/iPad/i)) || Boolean(userAgent.match(/iPhone/i));
-}
